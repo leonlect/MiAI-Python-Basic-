c56a90455aa411e11f1081cdd462dd6d5213e410
leonle <codebreakerct@gmail.com>
2021-06-13 17:50:43 +0700

    Add Bai 2 Mi AI

---
+++ Bai 1/demow3school.py
@@ -0,0 +1,10 @@
+# Unpack list demo
+# departments = ['CNTT', 'HTTT', 'KHMT', 'TT&MMT', 'KTPM']
+# anh, tuan, phat, chanh, bang = departments
+# print(anh)
+# print(tuan)
+# print(phat)
+# print(chanh)
+# print(bang)
+
+#
